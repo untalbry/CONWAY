11e166316e38e3769fa14fead73c9e3625b19370
bryancsinformatica <bryancsinformatica@gmail.com>
2024-10-20 19:15:26 -0600

    add(Pause game)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 width, height = 1280, 720
 bg = 25, 25, 25
 running = True
-
+pause = False
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -27,42 +27,41 @@ gameState[0,3]=1
 
 #Main loop 
 while running:
-    #EXIT 
+    #Events 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.KEYDOWN:
+            pause = not pause
     newGameState = np.copy(gameState)
     screen.fill(bg)
     #Draw grid
     for y in range(0, num_rows):
-        for x in range(0, num_columns): 
-
-            num_neighbors = (
-                gameState[(y - 1) % num_rows, (x - 1) % num_columns] + 
-                gameState[(y - 1) % num_rows, (x) % num_columns] + 
-                gameState[(y - 1) % num_rows, (x + 1) % num_columns] + 
-                gameState[(y) % num_rows, (x - 1) % num_columns] + 
-                gameState[(y) % num_rows, (x + 1) % num_columns] + 
-                gameState[(y + 1) % num_rows, (x - 1) % num_columns] + 
-                gameState[(y + 1) % num_rows, (x) % num_columns] + 
-                gameState[(y + 1) % num_rows, (x + 1) % num_columns]
-            )
- 
-            # Rule 1: A death cell with 3 living neighbors cells can be revived
-            if gameState[y, x] == 0 and num_neighbors == 3:
-                newGameState[y, x] = 1 
-            # Rule 2: A living cell with less than 2 living neighboring cells or more than 3 living neighboring cells dies...
-            elif gameState[y,x] == 1 and (num_neighbors < 2 or num_neighbors > 3):
-                newGameState[y,x] = 0
-            
-            # Definition of a polygon    
+        for x in range(0, num_columns):
+            if not pause: 
+                num_neighbors = (
+                    gameState[(y - 1) % num_rows, (x - 1) % num_columns] + 
+                    gameState[(y - 1) % num_rows, (x) % num_columns] + 
+                    gameState[(y - 1) % num_rows, (x + 1) % num_columns] + 
+                    gameState[(y) % num_rows, (x - 1) % num_columns] + 
+                    gameState[(y) % num_rows, (x + 1) % num_columns] + 
+                    gameState[(y + 1) % num_rows, (x - 1) % num_columns] + 
+                    gameState[(y + 1) % num_rows, (x) % num_columns] + 
+                    gameState[(y + 1) % num_rows, (x + 1) % num_columns]
+                )
+        
+                # Rule 1: A death cell with 3 living neighbors cells can be revived
+                if gameState[y, x] == 0 and num_neighbors == 3:
+                    newGameState[y, x] = 1 
+                # Rule 2: A living cell with less than 2 living neighboring cells or more than 3 living neighboring cells dies...
+                elif gameState[y,x] == 1 and (num_neighbors < 2 or num_neighbors > 3):
+                    newGameState[y,x] = 0
+                # Definition of a polygon    
             poly = [((x)    *   cell_size, y       * cell_size),
                     ((x+1)  *   cell_size, y       * cell_size),
                     ((x+1)  *   cell_size, (y+1)   * cell_size),
                     ((x)    *   cell_size, (y+1)   * cell_size)
             ]
-
             #Drawing things 
             if newGameState[y,x] == 0:
                 pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
